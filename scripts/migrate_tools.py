f1ed6cf7048599052941d0eccd637fe7a548d618
Matthew Zhou <mattzh1314@gmail.com>
2025-01-10 15:31:04 -1000

    chore: Remove `deprecated_tool` mentions everywhere (#598)

--- scripts/migrate_tools.py
+++ scripts/migrate_tools.py
@@ -4,11 +4,6 @@ from letta.schemas.user import User
 from letta.services.organization_manager import OrganizationManager
 from letta.services.tool_manager import ToolManager
 
-
-def deprecated_tool():
-    return "this is a deprecated tool, please remove it from your tools list"
-
-
 orgs = OrganizationManager().list_organizations(cursor=None, limit=5000)
 for org in tqdm(orgs):
     if org.name != "default":
