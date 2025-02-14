014a07f6fa8e5a7f6724fd189b33393f822c7ff1
Sarah Wooders <sarahwooders@gmail.com>
2025-02-13 16:45:21 -0800

    fix: add tests for vertex and add file  (#994)

---
+++ letta/llm_api/google_constants.py
@@ -0,0 +1,12 @@
+GOOGLE_MODEL_TO_CONTEXT_LENGTH = {
+    "gemini-2.0-flash-001": 1048576,
+    "gemini-2.0-pro-exp-02-05": 2097152,
+    "gemini-2.0-flash-lite-preview-02-05": 1048576,
+    "gemini-2.0-flash-thinking-exp-01-21": 1048576,
+    "gemini-1.5-flash": 1048576,
+    "gemini-1.5-pro": 2097152,
+    "gemini-1.0-pro": 32760,
+    "gemini-1.0-pro-vision": 16384,
+}
+
+GOOGLE_EMBEDING_MODEL_TO_DIM = {"text-embedding-005": 768, "text-multilingual-embedding-002": 768}
