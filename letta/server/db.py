465078cf83b2e97a328485c04e5f263725641ed8
cthomas <caren@letta.com>
2025-05-26 20:56:57 -0700

    feat: turn off prepared statements (#2444)

--- letta/server/db.py
+++ letta/server/db.py
@@ -1,5 +1,6 @@
 import os
 import threading
+import uuid
 from contextlib import asynccontextmanager, contextmanager
 from typing import Any, AsyncGenerator, Generator
 
@@ -164,6 +165,15 @@ class DatabaseRegistry:
             )
             if not is_async:
                 base_args["pool_use_lifo"] = settings.pool_use_lifo
+
+        elif is_async:
+            base_args.update(
+                {
+                    "prepared_statement_name_func": lambda: f"__asyncpg_{uuid.uuid4()}__",
+                    "statement_cache_size": 0,
+                    "prepared_statement_cache_size": 0,
+                }
+            )
 
         return base_args
 
