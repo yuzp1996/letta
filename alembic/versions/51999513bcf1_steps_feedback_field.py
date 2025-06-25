e2d2dad183e7b27eb935182a0255465f3104d7df
Andy Li <55300002+cliandy@users.noreply.github.com>
2025-06-25 16:40:56 -0700

    fix: alembic (#3036)

--- alembic/versions/51999513bcf1_steps_feedback_field.py
+++ alembic/versions/51999513bcf1_steps_feedback_field.py
@@ -14,7 +14,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "51999513bcf1"
-down_revision: Union[str, None] = "61ee53ec45a5"
+down_revision: Union[str, None] = "c7ac45f69849"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
