a6de0f203ed1287694aebd52ed3015208ffd388f
Kevin Lin <klin5061@gmail.com>
2025-07-10 21:45:00 -0700

    feat: remove persona/human from tool descriptions (#3250)
    Co-authored-by: Charles Packer <packercharles@gmail.com>

--- letta/services/tool_executor/core_tool_executor.py
+++ letta/services/tool_executor/core_tool_executor.py
@@ -188,7 +188,7 @@ class LettaCoreToolExecutor(ToolExecutor):
         Append to the contents of core memory.
 
         Args:
-            label (str): Section of the memory to be edited (persona or human).
+            label (str): Section of the memory to be edited.
             content (str): Content to write to the memory. All unicode (including emojis) are supported.
 
         Returns:
@@ -214,7 +214,7 @@ class LettaCoreToolExecutor(ToolExecutor):
         Replace the contents of core memory. To delete memories, use an empty string for new_content.
 
         Args:
-            label (str): Section of the memory to be edited (persona or human).
+            label (str): Section of the memory to be edited.
             old_content (str): String to replace. Must be an exact match.
             new_content (str): Content to write to the memory. All unicode (including emojis) are supported.
 
