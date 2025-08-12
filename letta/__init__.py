d9e4fb6be055d112b2c7738bdf8eeda7cb809f42
Caren Thomas <carenthomas@gmail.com>
2025-08-11 17:16:21 -0700

    poetry lock

--- letta/__init__.py
+++ letta/__init__.py
@@ -5,7 +5,7 @@ try:
     __version__ = version("letta")
 except PackageNotFoundError:
     # Fallback for development installations
-    __version__ = "0.11.2"
+    __version__ = "0.11.3"
 
 if os.environ.get("LETTA_VERSION"):
     __version__ = os.environ["LETTA_VERSION"]
