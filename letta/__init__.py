3c9e135a7914c351a838fed51aed09a4853fb3df
Caren Thomas <carenthomas@gmail.com>
2025-08-20 14:19:26 -0700

    bump version

--- letta/__init__.py
+++ letta/__init__.py
@@ -5,7 +5,7 @@ try:
     __version__ = version("letta")
 except PackageNotFoundError:
     # Fallback for development installations
-    __version__ = "0.11.3"
+    __version__ = "0.11.4"
 
 if os.environ.get("LETTA_VERSION"):
     __version__ = os.environ["LETTA_VERSION"]
